d24f1b6f04d1cf3c32ff50cb3cdc12b9b2ecdee1
Muhammad Sahal Nurdin <sahalnurdin@gmail.com>
2023-10-14 21:45:58 +0700

    Add Link Component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,26 @@
 /** @format */
 
+import Link from "next/link";
+
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body>
-        <header>[Header]</header>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link href='/'>Home</Link>
+              </li>
+              <li>
+                <Link href='/reviews'>Reviews</Link>
+              </li>
+              <li>
+                <Link href='/about'>About</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
         <main>{children}</main>
         <footer>[Footer]</footer>
       </body>
