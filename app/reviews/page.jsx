4d8dd5f934954ba6f8de596e3db11fac85a3941f
Muhammad Sahal Nurdin <sahalnurdin@gmail.com>
2023-10-14 22:15:00 +0700

    Adding some links in the review page

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -1,10 +1,28 @@
 /** @format */
+import Link from "next/link";
 
 export default function ReviewsPage() {
   return (
     <>
       <h1>Reviews</h1>
-      <p>Here we'll list all the reviews.</p>
+      <p>
+        Halaman review desain UI/UX menyajikan katalog varian desain kreatif,
+        dari tata letak hingga warna, memberikan pemahaman mendalam tentang
+        konsep dan kelebihan setiap proyek. Dengan analisis yang tajam, halaman
+        ini memandu pengunjung melalui inspirasi dan tren terkini, memberikan
+        wawasan kritis untuk pengembangan desain yang efektif. Diperkaya dengan
+        galeri visual, halaman ini menjadi sumber daya penting bagi desainer
+        yang ingin mengeksplorasi, mengamati, dan memahami esensi desain UI/UX
+        terbaik.
+      </p>
+      <ul>
+        <li>
+          <Link href='/reviews/office-attendance'>Office Attendance</Link>
+        </li>
+        <li>
+          <Link href='/reviews/coffee-shop'>Coffee Shop</Link>
+        </li>
+      </ul>
     </>
   );
 }
