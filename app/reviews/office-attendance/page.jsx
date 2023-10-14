79a14b676297b544732a8fe288ec9da955d3a4f1
Muhammad Sahal Nurdin <sahalnurdin@gmail.com>
2023-10-14 21:30:59 +0700

    Add More Pages

---
+++ app/reviews/office-attendance/page.jsx
@@ -0,0 +1,31 @@
+/** @format */
+
+export default function OfficeAttendancePage() {
+  return (
+    <>
+      <h1>Office Attendance</h1>
+      <p>
+        Desain Aplikasi Absensi Kantor ini menonjol dengan kecerdasan dalam
+        menyatukan fungsi dan estetika, menghadirkan pengalaman manajemen
+        kehadiran yang efisien dan elegan. Antarmukanya yang intuitif membuat
+        navigasi melalui platform ini menjadi pengalaman yang menyenangkan bagi
+        pengguna. Palet warna kontemporer yang dipilih dengan hati-hati
+        menciptakan suasana yang tenang namun profesional, mencerminkan nuansa
+        progresif di tempat kerja. Keunggulan desain responsifnya memastikan
+        aksesibilitas yang mulus di berbagai perangkat, memberikan karyawan
+        kemampuan untuk mencatat kehadiran dengan fleksibilitas. Fitur
+        pemantauan kehadiran real-time disajikan dengan jelas, memberikan
+        transparansi dan memberdayakan karyawan untuk mengelola jadwal mereka
+        secara efektif. Penggunaan ikon yang terpilih dengan bijak menambah
+        nilai pada aspek visual dan mempermudah penggunaan. Formulir minimalis
+        untuk pencatatan kehadiran memberikan keseimbangan antara sederhana dan
+        komprehensif. Integrasi kalender yang cerdas memungkinkan perencanaan
+        kehadiran yang efisien, sementara dasbor yang difokuskan pada karyawan
+        memberikan wawasan mendalam tentang riwayat kehadiran dan cuti.
+        Keseluruhan, desain ini menciptakan lingkungan di mana manajemen
+        kehadiran menjadi lebih dari tugas rutin — ia menjadi bagian yang
+        harmonis dari pengalaman kerja modern.
+      </p>
+    </>
+  );
+}
