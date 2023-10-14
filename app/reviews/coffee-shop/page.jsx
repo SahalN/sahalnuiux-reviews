002c2cbc4365247f5358e42aa3b53ca5aa7912fd
Muhammad Sahal Nurdin <sahalnurdin@gmail.com>
2023-10-14 21:25:15 +0700

    Add Page Routes

---
+++ app/reviews/coffee-shop/page.jsx
@@ -0,0 +1,37 @@
+/** @format */
+
+export default function CoffeeShopPage() {
+  return (
+    <>
+      <h1>Coffee Shop</h1>
+      <p>
+        Review: [Nama Coffee Shop] menghadirkan pengalaman yang menggoda dan
+        memikat bagi para pecinta kopi dengan desainnya yang penuh gaya. Begitu
+        melangkah masuk, suasana hangat dan nyaman langsung menyambut,
+        menciptakan tempat yang ideal untuk menikmati kopi dengan tenang. Desain
+        interior kafe ini sangat menggambarkan sentuhan yang modern dan elegan.
+        Dengan palet warna hangat seperti cokelat tua, krem, dan nuansa kayu
+        alami, [Nama Coffee Shop] memberikan nuansa yang cozy dan mengundang.
+        Furnitur yang dipilih dengan cermat, seperti kursi-kursi yang empuk dan
+        meja kayu yang indah, menciptakan ruang yang menyenangkan untuk
+        bersantai. Salah satu fitur menarik adalah dinding kaca yang
+        memungkinkan cahaya alami memasuki ruangan, menciptakan suasana terang
+        dan menyegarkan. Tanaman hias yang ditempatkan dengan apik di seluruh
+        kafe menambahkan sentuhan hijau yang membangkitkan semangat. Desain menu
+        dan tata letak area pemesanan juga patut dicatat. Konsepnya simpel dan
+        mudah dipahami, memudahkan pelanggan untuk menelusuri beragam kopi dan
+        hidangan ringan yang ditawarkan. Papan tulis dengan tulisan tangan
+        menampilkan menu spesial dan rekomendasi barista menambahkan sentuhan
+        personal yang menyenangkan. Selain itu, ruang pameran karya seni lokal
+        di dinding kafe memberikan sentuhan budaya dan mendukung komunitas
+        setempat. Ini tidak hanya menciptakan suasana yang berbeda, tetapi juga
+        memberikan pelanggan pengalaman yang lebih dalam saat menikmati
+        secangkir kopi. Keseluruhan, [Nama Coffee Shop] adalah kombinasi yang
+        harmonis antara desain modern dan hangat, menciptakan lingkungan yang
+        ideal untuk merayakan kopi dengan gaya. Dengan sentuhan artistik dan
+        perhatian pada detail, kafe ini berhasil memberikan pengalaman yang
+        tidak hanya memuaskan selera kopi, tetapi juga mata dan jiwa.
+      </p>
+    </>
+  );
+}
