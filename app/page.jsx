343e0ba808dcded7787f6212ada13a6522a9b95c
Muhammad Sahal Nurdin <sahalnurdin@gmail.com>
2023-10-14 21:15:05 +0700

    Add HomePage

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+/** @format */
+
+export default function HomePage() {
+  return (
+    <>
+      <h1>Sahal Nurdin</h1>
+      <p>
+        Selamat datang di Website Saya, tempat di mana desain UI/UX dieksplorasi
+        dengan cermat untuk memberikan ulasan mendalam dan inspirasi kreatif
+        bagi para pengembang dan desainer.{" "}
+      </p>
+    </>
+  );
+}
